27aca23f389982eb51a9b60bf9ca92585dd5cb5c
Artem <ZherebtsovAA@yandex.ru>
2023-04-04 12:29:14 +0900

    feat: create FilmDescriptionValidator, ValidateFilmDescriptionException class

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidateFilmDescriptionException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ValidateFilmDescriptionException extends ValidateException {
+    public ValidateFilmDescriptionException(final String message) {
+        super(message);
+    }
+}
