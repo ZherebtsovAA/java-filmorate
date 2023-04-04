e9d97bdd20216b47a5bf2a8d43581dbc7d68b000
Artem <ZherebtsovAA@yandex.ru>
2023-04-04 15:53:45 +0900

    feat: add logging for operations FilmController class

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.validators.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private final Map<String, Film> films; // Key - name, Value - Film
     private final List<FilmValidator> filmValidators = List.of(new FilmNameValidator(), new FilmDescriptionValidator()
@@ -22,31 +25,40 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film add(@RequestBody Film film) {
+    public Film add(@RequestBody Film film, HttpServletRequest request) {
         try {
+            log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}', Фильм: '{}'"
+                    , request.getMethod(), request.getRequestURI(), request.getQueryString(), film);
             checkValidatorRules(filmValidators, film);
             films.put(film.getName(), film);
             return film;
         } catch (ValidateFilmNameException exception) {
+            log.warn("Ошибка валидации имени: '{}'", exception.getMessage());
             System.out.println("Ошибка валидации имени: " + exception.getMessage());
             return film;
         } catch (ValidateFilmDescriptionException exception) {
+            log.warn("Ошибка валидации описания: '{}'", exception.getMessage());
             System.out.println("Ошибка валидации описания: " + exception.getMessage());
             return film;
         } catch (ValidateFilmReleaseDateException exception) {
+            log.warn("Ошибка валидации даты релиза: '{}'", exception.getMessage());
             System.out.println("Ошибка валидации даты релиза: " + exception.getMessage());
             return film;
         } catch (ValidateFilmDurationException exception) {
+            log.warn("Ошибка валидации продолжительности фильма: '{}'", exception.getMessage());
             System.out.println("Ошибка валидации продолжительности фильма: " + exception.getMessage());
             return film;
         } catch (ValidateException exception) {
+            log.warn("Ошибка валидации: '{}'", exception.getMessage());
             System.out.println("Ошибка валидации: " + exception.getMessage());
             return film;
         }
     }
 
     @PutMapping
-    public Film update(@RequestBody Film film) {
+    public Film update(@RequestBody Film film,  HttpServletRequest request) {
+        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}', Фильм: '{}'"
+                , request.getMethod(), request.getRequestURI(), request.getQueryString(), film);
         films.put(film.getName(), film);
         return film;
     }
